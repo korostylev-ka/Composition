3d94cf61a9b271efa6d6d36cebd8e56fe074e3f8
k.korostylev <k.korostylev@gmail.com>
2024-10-22 08:25:00 +0500

    Parcelable

--- app/src/main/java/com/korostylev/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/korostylev/composition/presentation/GameFinishedFragment.kt
@@ -1,5 +1,6 @@
 package com.korostylev.composition.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -43,6 +44,9 @@ class GameFinishedFragment : Fragment() {
                 retryGame()
             }
         })
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
     }
 
     override fun onDestroyView() {
@@ -51,8 +55,9 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
-
+        requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
+            gameResult = it
+        }
     }
 
     private fun retryGame() {
@@ -67,8 +72,7 @@ class GameFinishedFragment : Fragment() {
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_GAME_RESULT, gameResult)
-
+                    putParcelable(KEY_GAME_RESULT, gameResult)
                 }
             }
 
--- app/src/main/java/com/korostylev/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/korostylev/composition/presentation/GameFragment.kt
@@ -38,7 +38,9 @@ class GameFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+            level = it
+        }
 
     }
 
@@ -57,8 +59,7 @@ class GameFragment : Fragment() {
         fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_LEVEL, level)
-
+                    putParcelable(KEY_LEVEL, level)
                 }
             }
 
